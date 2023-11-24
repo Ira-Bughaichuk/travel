b6fe88428034a67a069d4aa7fcd020548402ddae
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-11-24 14:02:50 +0200

    fixed impport

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link"
-import LogoIcon from "/public/assets/icons/Logo.svg";
+import LogoIcon from "../public/assets/icons/Logo.svg";
 
 export default function Logo() {
   return (
