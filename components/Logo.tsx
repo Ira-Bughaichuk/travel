1f80d61538676ee8735d27b7f9cdc44e884c3414
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-11-24 17:13:11 +0200

    fixed logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import Link from "next/link"
-import logo from '/public/assets/icons/Logo.svg';
 export default function Logo() {
   return (
     <Link href="/" target='_blank' rel="noopener noreferrer nofollow" role="navigation" aria-label="Main" className="flex gap-[2px] flex-col w-[61px] [h-34px]">
        <Image
-         src={logo}
+         src="/icons/Logo.svg"
          alt="CarpTravel logo"
          width={59}
          height={21}
