bb638b399c94e2b76c9efc1d826c12dc3c782479
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-11-24 14:07:12 +0200

    fixed import

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link"
-import LogoIcon from "../public/assets/icons/Logo.svg";
-
+import LogoIcon from "@/public/assets/icons/Logo.svg";
+ //className="flex w-[59px] h-[21px] desktop:w-[61px] desktop:h-[22px]"
 export default function Logo() {
   return (
     <Link href="/" target='_blank' rel="noopener noreferrer nofollow" role="navigation" aria-label="Main" className="flex gap-[2px] flex-col w-[61px] [h-34px]">
@@ -10,7 +10,6 @@ export default function Logo() {
          alt="CarpTravel logo"
          width={61}
          height={22}
-         //className="flex w-[59px] h-[21px] desktop:w-[61px] desktop:h-[22px]"
           />
     </Link>
   );
