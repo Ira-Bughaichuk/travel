65343f1da7b5239d8b796b79fafe5ba62755f05a
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2023-11-24 17:37:00 +0200

    fixed logo

--- app/sections/Header.tsx
+++ app/sections/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
 
   return (
     <header className="absolute left-0 top-0 bg-transparent w-full">
-        <div className="flex  items-center justify-between mx-auto max-w-screen-xs
+        <div className="flex items-center justify-between mx-auto max-w-screen-xs
             tablet:max-w-screen-tablet tablet:w-[768px]
             desktop:max-w-screen-desktop desktop:w-[1280px]  py-[36px] px-[20px] md:w-auto w-full tablet:py-[25px] tablet:px-[32px] desktop:py-[24px] desktop:px-[104px] ">  
           <Logo/>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link"
-import logoImg from '/public/assets/icons/Logo.svg';
+import logoImg from '../public/assets/icons/Logo.svg';
 export default function Logo() {
   return (
     <Link href="/" target='_blank' rel="noopener noreferrer nofollow" role="navigation" aria-label="Main" className="flex gap-[2px] flex-col w-[61px] [h-34px]">
